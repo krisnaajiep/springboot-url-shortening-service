978c8bd4d82e98a4c5fdbd1f670019348c7b1d80
Krisna Ajie <krisnaajiep@gmail.com>
2026-06-03 15:12:27 +0700

    feat(shortening): implement create method in `ShortUrlService` for creating short url with Base62
    - Add DTOs for short url request and response.
    - Add `base62` dependency for Base62 encoding.
    - Add related configurations and exceptions.
    - Add `UrlShortener` interface with `Base62UrlShortener` implementation strategy.
    - Add associated unit tests.

---
+++ src/main/java/com/krisnaajiep/urlshortening/controller/InternalServerErrorException.java
@@ -0,0 +1,10 @@
+package com.krisnaajiep.urlshortening.controller;
+
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
